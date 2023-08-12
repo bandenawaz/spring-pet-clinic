ac09e161c71783af9a54bf7bc97bae1f64a8363d
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-08-12 09:59:43 +0530

    Initial Setup

---
+++ src/test/java/com/illuminateskills/spring/devops_automation/api/SpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illuminateskills.spring.devops_automation.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
