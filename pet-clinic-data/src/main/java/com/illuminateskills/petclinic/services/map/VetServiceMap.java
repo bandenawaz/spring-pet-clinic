7d3e6fa63c77d045378d1fc4c8136916b484cc31
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-08-13 18:47:28 +0530

    implemented map based service. Closes #6

---
+++ pet-clinic-data/src/main/java/com/illuminateskills/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,45 @@
+package com.illuminateskills.petclinic.services.map;
+
+import java.util.Set;
+
+import com.illuminateskills.petclinic.model.Vet;
+import com.illuminateskills.petclinic.services.CrudService;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long>{
+
+	@Override
+	public Set<Vet> findAll() {
+		// TODO Auto-generated method stub
+		return super.findAll();
+	}
+
+	@Override
+	public Vet findById(Long id) {
+		// TODO Auto-generated method stub
+		return super.findById(id);
+	}
+
+	/*
+	 * @Override Vet save(Long id, Vet object) { // TODO Auto-generated method stub
+	 * return super.save(id, object); }
+	 */
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Vet object) {
+		// TODO Auto-generated method stub
+		super.delete(object);
+	}
+
+	@Override
+	public Vet save(Vet object) {
+		// TODO Auto-generated method stub
+		return super.save(object.getId(), object);
+	}
+
+}
