20aff288f993058d8d7e388b066d1ace8538f7f5
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-08-18 10:07:34 +0530

    Fiexd i18n . Closes #25

--- pet-clinic-data/src/main/java/com/illuminateskills/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/illuminateskills/petclinic/services/OwnerService.java
@@ -2,10 +2,13 @@ package com.illuminateskills.petclinic.services;
 
 import com.illuminateskills.petclinic.model.Owner;
 import com.illuminateskills.petclinic.model.Person;
+import org.springframework.context.annotation.Bean;
 
 public interface OwnerService<O extends Person, L extends Number> extends CrudService<Owner, Long>{
 
     //Owner findById(Long id);
+
+
 
     Owner findByLastName(String lastName);
 
